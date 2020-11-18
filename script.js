6df9ed3ac787ce42431b58a12de6abf6576bea62
SR2090 <personalidmk3@gmail.com>
2020-11-18 11:32:43 +0530

    Removed console.log

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const switchPlayer = function () {
   player1EL.classList.toggle("player--active");
 };
 // starting condition
-console.log(diceEL);
+// console.log(diceEL);
 score0EL.textContent = 0;
 score1EL.textContent = 0;
 diceEL.classList.add("hidden");
@@ -40,7 +40,7 @@ diceEL.classList.add("hidden");
 btnRoll.addEventListener("click", function () {
   // 1. generate a random number
   const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  // console.log(dice);
   // 2. display dice
   diceEL.classList.remove("hidden");
   diceEL.src = `dice-${dice}.png`;
@@ -64,7 +64,7 @@ btnHold.addEventListener("click", function () {
   // TODO
   // 1. Add current score to active player score
   scores[activePlayer] += currentScore;
-  console.log(scores[activePlayer]);
+  // console.log(scores[activePlayer]);
   // scores[1] += currentScore
   document.getElementById(`score--${activePlayer}`).textContent =
     scores[activePlayer];
