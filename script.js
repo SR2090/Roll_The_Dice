ce889167997603816a64638bb51878d81f50938e
SR2090 <personalidmk3@gmail.com>
2020-11-18 10:28:30 +0530

    Added current player js query selector player--0

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 "use strict";
+// selecting the current player
+const player0EL = document.querySelector(".player--0");
+const player1EL = document.querySelector(".player--1");
 // selecting player score and dice image
+
 const score0EL = document.querySelector("#score--0");
 const score1EL = document.getElementById("score--1");
 const current0EL = document.getElementById("current--0");
