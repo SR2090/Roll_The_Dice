2dc7a41d3ef888d0575e09d4bd7de6993c4573f2
SR2090 <personalidmk3@gmail.com>
2020-11-18 09:43:07 +0530

    Roll dice function skeleton

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-'use strict';
+"use strict";
 // selecting player score and dice image
-const score0EL = document.querySelector('#score--0');
-const score1EL = document.getElementById('score--1');
-const diceEL = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+const score0EL = document.querySelector("#score--0");
+const score1EL = document.getElementById("score--1");
+const diceEL = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 // starting condition
 console.log(diceEL);
 score0EL.textContent = 0;
 score1EL.textContent = 0;
-diceEL.classList.add('hidden');
+diceEL.classList.add("hidden");
 
 // Rolling of dice
+btnRoll.addEventListener("click", function () {
+  // 1. generate a random number
+  // 2. display dice
+  // 3. check for 1
+  // 4. if true switch to next player
+});
